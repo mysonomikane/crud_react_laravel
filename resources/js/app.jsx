b01ae7bde8fdf70fece01210e20ddf1587843f1e
Abderrahman MOULAY ALI FRATT <mou.ali.abd@gmail.com>
2023-03-09 15:28:33 +0100

    KRID-48 Highlight Active Link in React

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -5,7 +5,7 @@ import  {BrowserRouter, Routes, Route} from "react-router-dom";
 import Tasks from "./components/tasks/Tasks";
 import Create from "./components/tasks/Create";
 import Edit from "./components/tasks/Edit";
-import Header from "./components/Header>";
+import Header from "./components/Header";
 
 ReactDOM.createRoot(document.getElementById("app")).render(
     <div className="row">
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById("app")).render(
             <BrowserRouter>
                 <Header />
                 <Routes>
-                    <Route path="/" exact element={<Header />} />
+                    <Route path="/" exact element={<Tasks />} />
                     <Route path="/create" exact element={<Create />} />
                     <Route path="/edit/:taskId" exact element={<Edit />} />
                 </Routes>
--- resources/js/components/Header.jsx
+++ resources/js/components/Header.jsx
@@ -1,7 +1,28 @@
 import  React from 'react'
+import {Link, useLocation} from "react-router-dom";
 
 export default function Header() {
+    const location = useLocation();
     return (
-        <div>Header</div>
+        <nav className="navbar navbar-expand-lg bg-light">
+            <div className="container-fluid">
+                <Link className="navbar-brand" to="/">Laravel CRUD React App</Link>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <Link className={`nav-link ${location.pathname === '/' ? 'active' : ''}`} aria-current="page" to="/"><i className="fas fa-home"></i> Home</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className={`nav-link ${location.pathname === '/create' ? 'active' : ''}`} to="/create"><i className="fas fa-edit"></i> Create Task</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
     )
 }
