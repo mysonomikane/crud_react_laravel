db8cede636bdeb6a04d773522d5a15e18eec236f
Abderrahman MOULAY ALI FRATT <mou.ali.abd@gmail.com>
2023-03-10 15:56:02 +0100

    KRID-49 Create TaskController

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    public function index(){
+        return Task::with('category')->paginate(10);
+    }
+
+    public function store(Request $request){
+        $task = Task::create([
+            'title' => $request->title,
+            'body' => $request->body,
+            'category_id' => $request->category_id
+        ]);
+        return $task;
+    }
+
+    public function show(Task $task){
+        return $task;
+    };
+
+    public function update(Request $request, Task $task){
+        $task->update([
+            'title' => $request->title,
+            'body' => $request->body,
+            'category_id' => $request->category_id,
+            'done' => $request->done
+        ]);
+        return $task;
+    }
+
+    public function destroy(Task $task){
+        $task->delete();
+        return ['message'=>"Your Task has been removed."];
+    }
+
+    public function getTaskByCategory(Category $category){
+        return $category->tasks()->with('category')->paginate(10);
+    }
+
+    public function getTasksOrderBy($column, $direction){
+        return Task::with('category')->orderBy($column, $direction)->paginate(10);
+    }
+
+    public function getTasksByTerm($term){
+        return Task::with('category')
+            ->where('title', 'like', '%'.$term.'%')
+            ->orWhere('body', 'like', '%'.$term.'%')
+            ->orWhere('id', 'like', '%'.$term.'%')
+            ->paginate(10);
+    }
+}
