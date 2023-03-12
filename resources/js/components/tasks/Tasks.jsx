43698fd08bb6c04a1521b4956e001e10fa54a0bb
Abderrahman MOULAY ALI FRATT <mou.ali.abd@gmail.com>
2023-03-12 22:45:26 +0100

    KRID-51 Adding Routes & Fetching Tasks

--- resources/js/components/tasks/Tasks.jsx
+++ resources/js/components/tasks/Tasks.jsx
@@ -1,6 +1,22 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import axios from "axios";
 
 export default function Tasks() {
+    const [tasks, setTasks] = useState([]);
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        fetchTasks();
+    }, []);
+
+    const fetchTasks = async () => {
+        try {
+            const response = await axios.get('/api/tasks');
+            setTasks(response.data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
     return (
         <div>Home</div>
     )
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\TaskController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +17,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('tasks', [TaskController::class, 'index']);
+Route::post('tasks', [TaskController::class, 'store']);
+Route::get('tasks/{id}', [TaskController::class, 'show']);
+Route::put('tasks/{id}', [TaskController::class, 'update']);
+Route::delete('tasks/{id}', [TaskController::class, 'destroy']);
+Route::get('category/{category}/tasks', [TaskController::class, 'getTaskByCategory']);
+Route::get('search/{term}/tasks', [TaskController::class, 'getTasksByTerm']);
+Route::get('order/{column}/{direction}/tasks', [TaskController::class, 'getTasksOrderBy']);
